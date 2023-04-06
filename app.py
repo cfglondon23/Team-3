c556af5255789a45a17d183f3157619a5379aa51
Sonny <69658757+sonnyzxc@users.noreply.github.com>
2023-04-06 17:51:47 +0100

    Finished leaderboard

--- app.py
+++ app.py
@@ -1,37 +1,29 @@
 from flask import Flask, render_template, redirect, request, url_for
 import json
-import air_pollution
+from flask_sqlalchemy import SQLAlchemy
+import data_calculations
+
 app = Flask(__name__, static_url_path='/static')
-# app.config['']
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///smartcity.db'
+db = SQLAlchemy(app)
 
+class CityDB(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    connectivity = db.Column(db.Float)
+    energy = db.Column(db.Float)
+    health_and_safety = db.Column(db.Float)
+    materials = db.Column(db.Float)
+    mobility = db.Column(db.Float)
+    placemaking = db.Column(db.Float)
 '''
-city worker
-community member
-different views 
-
-kensington
-canary wharf
-
 ---
 font: font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
 ---
-
-• focus on london 
-• people can have their own ID (tag London)
-• mix of text and images
-• scrollable??
-• notifications (change or progress)
 '''
-
-@app.route('/test')
-def test():
-    return render_template('index.html')
-
 @app.route('/', methods = ['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
-        print(request.form)
         if request.form['password'] == 'cityworker':
             return redirect(url_for('workerfeed'))
         elif request.form['password'] == 'communitymember':
@@ -40,36 +32,23 @@ def login():
             return "Invalid credentials"
     return render_template('login.html', error = error)
 
-@app.route('/workerfeed', methods=['GET', 'POST'])
+@app.route('/workerfeed')
 def workerfeed():
     error = None
-    if request.method == 'POST':
-        return redirect(url_for('city', cityname = request.form['query']))
-    return render_template('workerfeed2.html', error = error)
+    return render_template('feed.html', error = error)
 
-@app.route('/memberfeed', methods=['GET', 'POST'])
+@app.route('/notification')
 def memberfeed():
-    return 
+    return render_template('notification.html')
 
+@app.route('/leaderboards')
+def leaderboards():
+    return render_template('leaderboards.html')
 
-# dic = {
-#     "london" : [{
-#         "link" : "https://i.ytimg.com/vi/OX_6biwM-PY/maxresdefault.jpg",
-#         "caption" : "recycling",
-#         "account" : "John Doe"}]
-# , "bali" : [{
-#         "link" : "https://media.istockphoto.com/id/653953140/photo/hindu-temple-in-bali.jpg?s=612x612&w=0&k=20&c=ysj3S2kV1ZgCr4QZWDzjvHRowCI3-cR1xQNnqE8-BS4=",
-#         "caption" : "garden",
-#         "account" : "Dong"}]
-# }
-
-@app.route('/city/<string:cityname>')
-def city(cityname):
-    citydata = air_pollution.parsed_data['list'][0]['main']['aqi']
-    # data = dic[cityname] if cityname in dic else []
+@app.route('/user/<username>')
+def user(username):
     error = None
-    # return render_template('city.html', name = cityname, pictures = data, error = error)
-    return render_template('index.html', error = error, citydata = citydata)
+    return render_template(username + '.html', error = error)
 
 if __name__ == "__main__":
     app.run(debug=True)
